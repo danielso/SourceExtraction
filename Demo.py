894807b36b4a5bdabec1e46e25b149bf9fb2a005
danielso <daniel.soudry@gmail.com>
2016-11-17 19:17:03 +0200

    Changed Dataset in Demo

--- Demo.py
+++ Demo.py
@@ -6,8 +6,8 @@ def GetDefaultParams():
     # choose dataset name (function GetData will use this to fetch the correct dataset)
     data_name_set=['Hillman','HillmanSmall','Sophie2D','Sophie3D','SophieVoltage3D','Sophie3DSmall',
     'SaraSmall','Sara19DEC2015_w1t1','PhilConfocal','PhilMFM','PhilConfocal2','BaylorAxonsSmall',
-    'BaylorAxons','BaylorAxonsQuiet','BaylorAxonsActive']
-    data_name=data_name_set[1]
+    'BaylorAxons','BaylorAxonsQuiet','BaylorAxonsActive','BaylorAxonsJiakun1']
+    data_name=data_name_set[-2]
     
     # "default" parameters - for additional information see "LocalNMF" in BlockLocalNMF
     
@@ -32,7 +32,7 @@ def GetDefaultParams():
     Connected=False # should we constrain all spatial component to be connected?
     WaterShed=False # should we constrain all spatial component to have only one watershed component?
     MedianFilt=False # should we perfrom median filtering of shape?
-    FineTune=True # Should use the full data at the main iterations (to fine tune shapes)? If not then just extract the activity and not the shapes from the full data
+    FineTune=False # Should use the full data at the main iterations (to fine tune shapes)? If not then just extract the activity and not the shapes from the full data
     ThresholdData= True  # threshold data with PSD level
     
     # experimental stuff - don't use for now
@@ -50,14 +50,14 @@ def GetDefaultParams():
         mbs=[1] # temporal downsampling of data in intial phase of NMF
         ds=1 # spatial downsampling of data in intial phase of NMF. Ccan be an integer or a list of the size of spatial dimensions
         TargetAreaRatio=[0.005,0.02] # target sparsity range for spatial components
-        repeats=1 # how many repeations to run NMF algorithm
+        repeats=2 # how many repeations to run NMF algorithm
         iters0=[10] # number of intial NMF iterations, in which we downsample data and add components
-        iters=30 # number of main NMF iterations, in which we fine tune the components on the full data
+        iters=10 # number of main NMF iterations, in which we fine tune the components on the full data
         lam1_s=0.1 # l1 regularization parameter initialization (for increased sparsity). If zero, we have no l1 sparsity penalty
         updateLambdaIntervals=2 # update sparsity parameter every updateLambdaIntervals iterations
         addComponentsIntervals=1 # in initial NMF phase, add new component every updateLambdaIntervals*addComponentsIntervals iterations
         updateRhoIntervals=1 # in main NMF phase, add new component every updateLambdaIntervals*updateRhoIntervals iterations
-        Background_num=1 #number of background components - one of which at every repetion
+        Background_num=2 #number of background components - one of which at every repetion
         bkg_per=0.2 # intialize of background shape at this percentile (over time) of video
         sig=(500,500,500) # estiamte size of neuron - bounding box is 3 times this size. If larger then data, we have no bounding box.
         
@@ -196,7 +196,7 @@ def GetDefaultParams():
         WaterShed=False # should we constrain all spatial component to have only one watershed component?        
         SigmaMask=3  
     elif data_name=='BaylorAxonsQuiet':
-        NumCent=50 # Max number of centers to import from Group Lasso intialization - if 0, we don't run group lasso
+        NumCent=400 # Max number of centers to import from Group Lasso intialization - if 0, we don't run group lasso
         mbs=[10] # temporal downsampling of data in intial phase of NMF
         ds=1 # spatial downsampling of data in intial phase of NMF. Ccan be an integer or a list of the size of spatial dimensions
         TargetAreaRatio=[0.01,0.06] # target sparsity range for spatial components
@@ -242,6 +242,35 @@ def GetDefaultParams():
         Connected=True # should we constrain all spatial component to be connected?
         WaterShed=False # should we constrain all spatial component to have only one watershed component?        
         SigmaMask=3  
+        
+    elif data_name=='BaylorAxonsJiakun1' :
+        NumCent=400 # Max number of centers to import from Group Lasso intialization - if 0, we don't run group lasso
+        mbs=[10] # temporal downsampling of data in intial phase of NMF
+        ds=1 # spatial downsampling of data in intial phase of NMF. Ccan be an integer or a list of the size of spatial dimensions
+        TargetAreaRatio=[0.01,0.06] # target sparsity range for spatial components
+        repeats=2 # how many repeations to run NMF algorithm
+        iters0=[30] # number of intial NMF iterations, in which we downsample data and add components
+        iters=100 # number of main NMF iterations, in which we fine tune the components on the full data
+        lam1_s=10# l1 regularization parameter initialization (for increased sparsity). If zero, we have no l1 sparsity penalty
+        updateLambdaIntervals=2 # update sparsity parameter every updateLambdaIntervals iterations
+        addComponentsIntervals=1 # in initial NMF phase, add new component every updateLambdaIntervals*addComponentsIntervals iterations
+        updateRhoIntervals=1 # in main NMF phase, update sparsity learning speed (Rho) every updateLambdaIntervals*updateRhoIntervals iterations
+        Background_num=1 #number of background components - one of which at every repetion
+        bkg_per=0.1 # intialize of background shape at this percentile (over time) of video
+        sig=(200,200) # estiamte size of neuron - bounding box is 3 times this size. If larger then data, we have no bounding box.
+        
+        ThresholdData= True  # threshold data with PSD level
+        FineTune=False
+        NonNegative=True # should we constrain activity and shapes to be non-negative?
+        FinalNonNegative=True # should we constrain activity to be non-negative at final iteration?
+        Connected=True # should we constrain all spatial component to be connected?
+        WaterShed=False # should we constrain all spatial component to have only one watershed component?        
+        SigmaMask=3  
+        
+       
+        
+        
+        
 
     params_dict=dict([['data_name',data_name],['SuperVoxelize',SuperVoxelize],['NonNegative',NonNegative],
                       ['FinalNonNegative',FinalNonNegative],['mbs',mbs],['TargetAreaRatio',TargetAreaRatio],
